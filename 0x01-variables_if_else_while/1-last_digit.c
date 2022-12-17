639a0af1b14f68394f0088eaf376a454300518b7
Jafermenza <jafer1432@gmail.com>
2022-12-17 11:31:19 -0800

    doublecode

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,67 +4,29 @@
 
 #include <time.h>
 
-
-
 /**
-
- * main - this function print using put
-
+ * main - Prints the last digit of a randomly generated number and whether igreater than 5, less than 6, or 0. 
  *
-
- * Return: Always 0 (Success)
-
+ * Return: Always 0.  
  */
-
-
-
+  
 int main(void)
-  
 {
-  
-  int n,ld;
-  
-
-  
-  srand(time(0));
-  
-  n = rand() - RAND_MAX / 2;
-  
-ld = n % 10;
-  
-  if (ld < 0)
-    
-    {
-      
-      ld=ld*-1;
-      
-    }
-  
-  if (ld > 5)
-    
-    {
-      
-      /* if condition is true then print the following */
-      
-      printf("Last digit of %d",n);
-      
-      printf(" is %d",ld);
-      
-      printf(" and is greater than 5\n");
-      
-    }
-  
-  else  if (ld == 0)    
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if ((n % 10) > 5)
 {
-printf("Last digit of %d",n);
-printf("is %d",ld);
-printf(" and is 0\n");
+printf("Last digit of %d is %d and is greater than 5\n",n, n % 10);
 }
-else if (ld < 5)    
+else if ((n % 10) < 6 && (n % 10) != 0)      
 {
-printf("Last digit of %d",n);
-printf(" is %d",ld);
-printf(" and is less than than 5\n");
+printf("Last digit of %d is %d and is less than 6 and not 0\n",n, n % 10);
+}    
+else
+{
+printf("Last digit of %d is %d and is 0\n",	       
+n, n % 10);
 }
 return (0);
 }
