8cc0da72b11a3114b8ea3ba7595798cb76a27b73
Jafermenza <jafer1432@gmail.com>
2022-12-17 05:49:22 -0800

    advact2

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this function print using put
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+return (1);
+}
