785fe1c482d746136a552c67cbb63b3b2088142b
Jafermenza <jafer1432@gmail.com>
2022-12-20 09:49:05 -0800

    adv tasks2

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -17,10 +17,10 @@ fibsum = fib1 + fib2;
 if (fibsum > 4000000)
 break;
 if ((fibsum % 2) == 0)
-tot_sum += fibsum
+tot_sum += fibsum;
 fib1 = fib2;
 fib2 = fibsum;
 }
-printf("%.0f\n", tot_sum)
+printf("%.0f\n", tot_sum);
 return (0);
 }
