bb4eb0b00c08eb1575a54bad835f70c0bf00c044
Jafermenza <jafer1432@gmail.com>
2022-12-20 11:35:22 -0800

    fixed main error

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -6,61 +6,32 @@
 #include <time.h>
 
 /**
-
  *main:Entry point 
-
  *main:world jdd
-
  *Short Description:documenation betty style for these f
-
  *Description:verify if the number is 0 or negative or positive
-
  *Return: Always 0 (Success)
-
  */
 
-int main(void)
-  
+void positive_or_negative (int n)
 {
   
-  /*this main function verify if numb is positive negitve or zero */
-  
-  int n;
-  
-  srand(time(0));
-  
-  n = rand() - RAND_MAX / 2;
-  
-  if (n > 0)
-    
-    {
-      
-      /* if condition is true then print the following */
-      
-      printf("%d is positive\n", n);
-      
-    }
-  
-  else  if (n < 0)
-    
-    {
-      
-      /* if condition is false then print the following */
-      
-      printf("%d is negative\n", n);
-      
-    }
-  
-  else if (n == 0)
-    
-    {
-      
-      /* if condition is false then print the following */
-      
-      printf("%d is zero\n", n);
-      
-    }
-  
-  return (0);
-  
+srand(time(0));
+n = rand() - RAND_MAX / 2; 
+if (n > 0)
+{      
+/* if condition is true then print the following */
+printf("%d is positive\n", n);
+}
+else  if (n < 0)
+{
+/* if condition is false then print the following */
+printf("%d is negative\n", n);
+}
+else if (n == 0)
+{
+/* if condition is false then print the following */
+printf("%d is zero\n", n);
+}
+ return;
 }
