2b14c9550be201f76e0193358e716b3000349cd4
Jafermenza <jafer1432@gmail.com>
2022-12-20 11:49:14 -0800

    fix main error

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,8 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
-
 #include <stdio.h>
-
 #include <time.h>
 
 /**
@@ -15,10 +13,7 @@
 
 void positive_or_negative (int n)
 {
-  
-srand(time(0));
-n = rand() - RAND_MAX / 2; 
-if (n > 0)
+ if (n > 0)
 {      
 /* if condition is true then print the following */
 printf("%d is positive\n", n);
