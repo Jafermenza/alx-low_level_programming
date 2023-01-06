1be6763a48368089c7b6cfb9330afb7defff32d4
Jafermenza <jafermenza@gmail.com>
2023-01-06 08:42:40 -0800

    pushing t1

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,9 +1,6 @@
 #include "main.h"
 
-
-
 /**
-
  * memset - Fills the first n bytes of the memory area
  *          pointed to by @s with the constant byte @c.
  * @s: A pointer to the memory area to be filled.
@@ -14,21 +11,14 @@
  */
 
 void *_memset(void *s, int c, size_t n)
-  
 {
-  
   unsigned int index;
-  
   unsigned char *memory = s, value = c;
-  
-
-  
   for (index = 0; index < n; index++)
-    
     memory[index] = value;
-  
+
 
   
   return (memory);
-  
+
 }
